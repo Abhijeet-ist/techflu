89992a0911120cabda1c4718515b2ac06b4f1352
Ansh Kumar <anshkumar1399@gmail.com>
2025-02-23 20:20:27 +0530

    hloo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import Hero from "@/components/Home/Hero";
+import { HeroParallaxDemo } from "@/components/Home/HeroParallaxDemo";
+import { TimelineDemo } from "@/components/Home/TimelineDemo";
+import { div } from "framer-motion/client";
 
 
 export default function Home() {
   return (
-    <Hero />
+    <div>
+      <div>
+        <Hero />
+      </div>
+      <div>
+        <TimelineDemo />
+      </div>
+      <div>
+        <HeroParallaxDemo />
+      </div>
+    </div>
   );
 }
