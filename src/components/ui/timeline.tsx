0fadb0e9d467acf3e769afd60125ad5fe8e2ef35
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-02-24 10:22:18 +0530

    Changes in css and texts

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -38,11 +38,11 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
             ref={containerRef}
         >
             <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-                <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-                    Changelog from my journey
+                <h2 className="text-lg md:text-4xl mb-4 font-bold text-orange-500 opacity-75 max-w-4xl">
+                    Changelog of TECHFLUENCE
                 </h2>
-                <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-                    I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
+                <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base max-w-sm italic leading-relaxed">
+                    It&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
                     a timeline of my journey.
                 </p>
             </div>
