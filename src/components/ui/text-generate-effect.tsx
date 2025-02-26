bd31d6d3fa76716849a764b40569e9604c1fcd6f
ANSH KUMAR <anshkumar1399@gmail.com>
2025-02-26 14:03:59 +0530

    ansh

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useEffect, useRef } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
+import { useEffect, useRef } from "react";
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 import { HeroStats } from "../Home/hero-stats";
 
@@ -38,7 +38,7 @@ export const TextGenerateEffect = ({
     return (
         <div className="bg-gradient-to-b from-black via-orange-950 to-black px-4 sm:px-8 py-16">
             <section className="relative flex items-center justify-center px-4 sm:px-8 py-16">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 ">
                     {/* LEFT SIDE - Animated Text */}
                     <motion.div ref={scope} className="text-left">
                         <h1 className="text-5xl sm:text-7xl md:text-8xl font-serif font-extrabold text-white mb-6 relative overflow-hidden">
@@ -70,13 +70,16 @@ export const TextGenerateEffect = ({
                         transition={{ duration: 1 }}
                     >
                         <div className="relative rounded-xl p-2 bg-gradient-to-r from-orange-500 via-yellow-300 to-orange-500 animate-borderPulse">
-                            <img
-                                src="/banner.jpeg"
-                                alt="TechFluence Event"
+                            <video
+                                src="https://scontent.cdninstagram.com/o1/v/t16/f2/m82/AQPl7boBHAFwQTSWC6wLGjUzIWhpJ0AhZQcS55VavvqRo3AQ9sJHbHmllbWidTawNhnUalmuBEw_MxmFcrvQmb3tsvXfY3_SEHt8WOY.mp4?efg=eyJ4cHZfYXNzZXRfaWQiOjEwNjA0NzcxOTg1NDEyMDUsInZlbmNvZGVfdGFnIjoieHB2X3Byb2dyZXNzaXZlLklOU1RBR1JBTS5DTElQUy5DMy43MjAuZGFzaF9iYXNlbGluZV8xX3YxIn0&_nc_ht=instagram.fixc2-1.fna.fbcdn.net&_nc_cat=106&vs=de354a73381bf1f4&_nc_vs=HBksFQIYT2lnX3hwdl9yZWVsc19wZXJtYW5lbnRfcHJvZC84NDQ3M0ZFMDlFODBENjJFNkZGQjQ5NzJBQkE3NzhBRF92aWRlb19kYXNoaW5pdC5tcDQVAALIAQAVAhg6cGFzc3Rocm91Z2hfZXZlcnN0b3JlL0dMdnJXeG5fdXRSelBOZ0RBUEpLSXY5aVc4TmZicV9FQUFBRhUCAsgBACgAGAAbAogHdXNlX29pbAExEnByb2dyZXNzaXZlX3JlY2lwZQExFQAAJqqGsevyn-IDFQIoAkMzLBdATjMzMzMzMxgSZGFzaF9iYXNlbGluZV8xX3YxEQB1_gcA&ccb=9-4&oh=00_AYDoRw3twrQtJMvMJ4_6dUxJbalBiVda43OQM9upa1uttg&oe=67C09012&_nc_sid=1d576d"
                                 className="rounded-xl shadow-lg border-2 border-transparent w-full max-w-xs sm:max-w-sm md:max-w-2xl"
+                                autoPlay
+                                loop
+                                muted
                             />
                         </div>
                     </motion.div>
+                    
                 </div>
             </section>
             <HeroStats />
